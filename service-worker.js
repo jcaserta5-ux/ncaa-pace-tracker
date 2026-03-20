f47ce485bc961c7b7e4435208dac1685586f2436
jcaserta5-ux <jcaserta5@gmail.com>
2026-03-20 09:51:31 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open("pace-cache").then(cache => {
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(resp => resp || fetch(event.request))
+);
+});
